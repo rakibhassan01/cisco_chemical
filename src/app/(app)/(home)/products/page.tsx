ff327a39fdc77624a7b1997faee45ce5ee0b95a9
RakibCoding <rhbappy456@gmail.com>
2025-09-17 20:54:28 +0600

    update

--- src/app/(app)/(home)/products/page.tsx
+++ src/app/(app)/(home)/products/page.tsx
@@ -1,9 +1,19 @@
-import { ProductsView } from "@/modules/products/ui/views/product-view";
+// import { ProductsView } from "@/modules/products/ui/views/product-view";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-const Products = () => {
+const Products = async () => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  const data = await payload.find({
+    collection: "categories",
+  });
   return (
     <div className="pt-18">
-      <ProductsView />
+      {/* <ProductsView /> */}
+      + 
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 };
--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -2,10 +2,12 @@ import type { CollectionConfig } from "payload";
 
 export const Categories: CollectionConfig = {
   slug: "categories",
-  admin: {
-    useAsTitle: "email",
-  },
-  auth: true,
   fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
   ],
 };
+
