1555c5baa268ce42cc60b4f6c71151a90cfa7276
rakibhassan01 <rhbappy456@gmail.com>
2025-11-18 23:06:44 +0600

    update

--- src/app/(app)/(home)/products/page.tsx
+++ src/app/(app)/(home)/products/page.tsx
@@ -1,15 +1,15 @@
 // import { ProductsView } from "@/modules/products/ui/views/product-view";
 import { ProductsView } from "@/modules/products/ui/views/product-view";
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
+// import configPromise from "@payload-config";
+// import { getPayload } from "payload";
 
 const Products = async () => {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-  const data = await payload.find({
-    collection: "categories",
-  });
+  // const payload = await getPayload({
+  //   config: configPromise,
+  // });
+  // const data = await payload.find({
+  //   collection: "categories",
+  // });
   return (
     <div className="pt-18">
       <ProductsView />+{/* {JSON.stringify(data, null, 2)}  */}
